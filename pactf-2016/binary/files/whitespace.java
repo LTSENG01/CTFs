da08df4eb025ed821175069225756939d352ef84
Larry Tseng <lt222888@gmail.com>
2016-06-08 16:42:57 -0400

    Add files and flags from PACTF

---
+++ pactf-2016/binary/files/whitespace.java
@@ -0,0 +1,67 @@
+public class whitespace {	 	 	  
+	
+    public static void main(String[] args) {   
+	
+     System.out.println("You	will	find the flag	when you	run");
+	
+     System.out.println("this program");	     
+	
+     System.out.println(obfuscate());		  		 
+	
+    } 		 		  
+	
+    public static String obfuscate() {   	
+	
+     int	i='w'	, j='s' ,	k='p'	,	l='a';
+	
+     i=i^7; 	     
+	
+     j=j>>1;		 	  	
+	
+     l=l<<1;			  		
+	
+     i-=2; 			 	 
+	
+     j+=54; 	     
+	
+     l-=93;			 	  
+	
+     int[] a = {i,j,k,l};		 			
+	
+     int b = new int[][]a; 				
+	
+    } 	 					
+	
+}     			    
+	
+     			  	 
+	
+     		 				
+	
+     		  			
+	
+     			  	 
+	
+     		    	
+	
+     		 		 	
+	
+     			  		
+	
+     	 					
+	
+     		 	  	
+	
+     		 			 
+	
+     	 					
+	
+     		 				
+	
+     		 			 
+	
+     		  	 	
+	
+  
+
+
